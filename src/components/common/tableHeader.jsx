d97a70af44b3b106b69203acb91f6ffe055f1e76
samiwokx <samsaravillo@gmail.com>
2020-11-03 20:00:38 +0800

    add new jsx tableHeader

---
+++ src/components/common/tableHeader.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class TableHeader extends Component {
+    raiseSort = path => { 
+        const sortColumn = {...this.props.sortColumn};
+        if (sortColumn.path === path) {
+            sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc';
+        } else {
+            sortColumn.path = path;
+            sortColumn.order = 'asc';
+        }
+        this.props.onSort(sortColumn);  
+    };
+
+    render() { 
+        return ('Test');
+    }
+}
+ 
+export default TableHeader;
