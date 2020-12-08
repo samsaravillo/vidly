9b20b105a9d98b48c9868ce376a2532bf6d109d7
samiwokx <samsaravillo@gmail.com>
2020-12-08 21:14:30 +0800

    add new component registerForm

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import MovieForm from './components/movieForm';
 import LoginForm from './components/loginForm';
+import RegisterForm from './components/registerForm';
 import './App.css';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
         <main className="container">
           <div className="content">
             <Switch>
+              <Route path="/register" component={RegisterForm} />
               <Route path="/login" component={LoginForm} />
               <Route path="/movies/:_id" component={MovieForm} />
               <Route path="/movies" component={Movies} />
