e8befe6783379c2f45fa789e7e908ba4f8b09de1
Sam <samsaravillo@gmail.com>
2025-11-07 15:21:45 -0800

    Update genreService.js

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -2,5 +2,12 @@ import http from './httpService';
 import config from '../config.json';
 
 export function getGenres() {
-  return http.get(config.apiUrl + '/genres');
+  //return http.get(config.apiUrl + '/genres');
+  return Promise.resolve({
+    data: [
+      { _id: '5b21ca3eeb7f6fbccd471818', name: 'Action' },
+      { _id: '5b21ca3eeb7f6fbccd471814', name: 'Comedy' },
+      { _id: '5b21ca3eeb7f6fbccd471820', name: 'Thriller' },
+    ],
+  });
 }
